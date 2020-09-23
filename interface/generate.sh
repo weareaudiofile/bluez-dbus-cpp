b78a4cbeeba480b15a8458b76906c5d7f30f52da
Nethanja Focking <nethanja@nethanja.de>
2020-09-23 21:42:04 +0300

    Fix generation and folder structure

--- interface/generate.sh
+++ interface/generate.sh
@@ -7,31 +7,34 @@
 #
 
 # output paths
-PROXY=../proxy
-ADAPTOR=../adaptor
+PROXY=../include/bluez-dbus-cpp/proxy
+ADAPTOR=../include/bluez-dbus-cpp/adaptor
+MESH_PROXY=../include/bluez-dbus-cpp/mesh/proxy
+MESH_ADAPTOR=../include/bluez-dbus-cpp/mesh/adaptor
+XML2CPP=sdbus-c++-xml2cpp
 
 # Clients
-sdbus-c++-xml2cpp Adapter1.xml --proxy=${PROXY}/Adapter1_proxy.h
-sdbus-c++-xml2cpp Media1.xml --proxy=${PROXY}/Media1_proxy.h
-sdbus-c++-xml2cpp NetworkServer1.xml --proxy=${PROXY}/NetworkServer1_proxy.h
-sdbus-c++-xml2cpp SimAccess1.xml --proxy=${PROXY}/SimAccess1_proxy.h
-sdbus-c++-xml2cpp AgentManager1.xml --proxy=${PROXY}/AgentManager1_proxy.h
-sdbus-c++-xml2cpp ProfileManager1.xml --proxy=${PROXY}/ProfileManager1_proxy.h
-sdbus-c++-xml2cpp HealthManager1.xml --proxy=${PROXY}/HealthManager1_proxy.h
+${XML2CPP} Adapter1.xml --proxy=${PROXY}/Adapter1_proxy.h
+${XML2CPP} Media1.xml --proxy=${PROXY}/Media1_proxy.h
+${XML2CPP} NetworkServer1.xml --proxy=${PROXY}/NetworkServer1_proxy.h
+${XML2CPP} SimAccess1.xml --proxy=${PROXY}/SimAccess1_proxy.h
+${XML2CPP} AgentManager1.xml --proxy=${PROXY}/AgentManager1_proxy.h
+${XML2CPP} ProfileManager1.xml --proxy=${PROXY}/ProfileManager1_proxy.h
+${XML2CPP} HealthManager1.xml --proxy=${PROXY}/HealthManager1_proxy.h
 
 # Services
-sdbus-c++-xml2cpp GattService1.xml --adaptor=${ADAPTOR}/GattService1_adaptor.h
-sdbus-c++-xml2cpp GattCharacteristic1.xml --adaptor=${ADAPTOR}/GattCharacteristic1_adaptor.h
-sdbus-c++-xml2cpp GattDescriptor1.xml --adaptor=${ADAPTOR}/GattDescriptor1_adaptor.h
+${XML2CPP} GattService1.xml --adaptor=${ADAPTOR}/GattService1_adaptor.h
+${XML2CPP} GattCharacteristic1.xml --adaptor=${ADAPTOR}/GattCharacteristic1_adaptor.h
+${XML2CPP} GattDescriptor1.xml --adaptor=${ADAPTOR}/GattDescriptor1_adaptor.h
 
 ## Mesh
 # Clients
-sdbus-c++-xml2cpp mesh/Management1.xml --proxy=${PROXY}/mesh/Management1_proxy.h
-sdbus-c++-xml2cpp mesh/Network1.xml --proxy=${PROXY}/mesh/Network1_proxy.h
-sdbus-c++-xml2cpp mesh/Node1.xml --proxy=${PROXY}/mesh/Node1_proxy.h
-sdbus-c++-xml2cpp mesh/ProvisionAgent1.xml --proxy=${PROXY}/mesh/ProvisionAgent1_proxy.h
-sdbus-c++-xml2cpp mesh/Provisioner1.xml --proxy=${PROXY}/mesh/Provisioner1_proxy.h
+${XML2CPP} mesh/Management1.xml --proxy=${MESH_PROXY}/Management1_proxy.h
+${XML2CPP} mesh/Network1.xml --proxy=${MESH_PROXY}/Network1_proxy.h
+${XML2CPP} mesh/Node1.xml --proxy=${MESH_PROXY}/Node1_proxy.h
+${XML2CPP} mesh/ProvisionAgent1.xml --proxy=${MESH_PROXY}/ProvisionAgent1_proxy.h
+${XML2CPP} mesh/Provisioner1.xml --proxy=${MESH_PROXY}/Provisioner1_proxy.h
 
 # Services
-sdbus-c++-xml2cpp mesh/Application1.xml --adaptor=${ADAPTOR}/mesh/Application1_adaptor.h
-sdbus-c++-xml2cpp mesh/Element1.xml --adaptor=${PROXY}/mesh/Element1_adaptor.h
+${XML2CPP} mesh/Application1.xml --adaptor=${MESH_ADAPTOR}/Application1_adaptor.h
+${XML2CPP} mesh/Element1.xml --adaptor=${MESH_ADAPTOR}/Element1_adaptor.h
