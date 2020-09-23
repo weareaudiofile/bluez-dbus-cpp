11c29ca885180d6d67418b69d7c6df22cf16b2f5
Nethanja Focking <nethanja@nethanja.de>
2020-09-23 20:53:35 +0300

    Generate new mesh interfaces

--- interface/generate.sh
+++ interface/generate.sh
@@ -23,3 +23,15 @@ sdbus-c++-xml2cpp HealthManager1.xml --proxy=${PROXY}/HealthManager1_proxy.h
 sdbus-c++-xml2cpp GattService1.xml --adaptor=${ADAPTOR}/GattService1_adaptor.h
 sdbus-c++-xml2cpp GattCharacteristic1.xml --adaptor=${ADAPTOR}/GattCharacteristic1_adaptor.h
 sdbus-c++-xml2cpp GattDescriptor1.xml --adaptor=${ADAPTOR}/GattDescriptor1_adaptor.h
+
+# Mesh
+# Clients
+sdbus-c++-xml2cpp mesh/Management1.xml --proxy=${PROXY}/mesh/Management1_proxy.h
+sdbus-c++-xml2cpp mesh/Network1.xml --proxy=${PROXY}/mesh/Network1_proxy.h
+sdbus-c++-xml2cpp mesh/Node1.xml --proxy=${PROXY}/mesh/Node1_proxy.h
+sdbus-c++-xml2cpp mesh/ProvisionAgent1.xml --proxy=${PROXY}/mesh/ProvisionAgent1_proxy.h
+sdbus-c++-xml2cpp mesh/Provisioner1.xml --proxy=${PROXY}/mesh/Provisioner1_proxy.h
+
+# Services
+sdbus-c++-xml2cpp mesh/Application1.xml --adaptor=${ADAPTOR}/mesh/Application1_adaptor.h
+sdbus-c++-xml2cpp mesh/Element1.xml --adaptor=${PROXY}/mesh/Element1_adaptor.h
